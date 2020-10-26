4b6e7359f21dc381726a1c0ad45866d2582fb72a
7dog123 <futuretrunks745@gmail.com>
2020-10-26 10:50:24 -0500

    fixed sndserver build error

--- sndserv/linux.c
+++ sndserv/linux.c
@@ -37,6 +37,7 @@ static const char rcsid[] = "$Id: linux.c,v 1.3 1997/01/26 07:45:01 b1 Exp $";
 
 #include <stdlib.h>
 #include <stdio.h>
+#includr <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -53,7 +54,6 @@ myioctl
   int*	arg )
 {   
     int		rc;
-    extern int	errno;
     
     rc = ioctl(fd, command, arg);  
     if (rc < 0)
