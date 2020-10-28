978eb716ce707f6d006cb8e1124cc0f5f5a0ba11
7dog123 <futuretrunks745@gmail.com>
2020-10-28 16:46:58 -0500

    Fix quit prompt not displayed properly

--- linuxdoom-1.10/m_menu.c
+++ linuxdoom-1.10/m_menu.c
@@ -1756,19 +1756,22 @@ void M_Drawer (void)
 	y = 100 - M_StringHeight(messageString)/2;
 	while(*(messageString+start))
 	{
+		int foundnewline = 0;
+		
 	    for (i = 0;i < strlen(messageString+start);i++)
 		if (*(messageString+start+i) == '\n')
 		{
 		    memset(string,0,40);
 		    strncpy(string,messageString+start,i);
+			foundnewline = 1;
 		    start += i+1;
 		    break;
 		}
 				
-	    if (i == strlen(messageString+start))
+	    if (!foundnewline)
 	    {
 		strcpy(string,messageString+start);
-		start += i;
+		start += strlen(string);
 	    }
 				
 	    x = 160 - M_StringWidth(string)/2;
